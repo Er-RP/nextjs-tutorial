3229cdc8b0f9fb78d85040ae28eb27bb780c9f03
Rajapandi <rajapandi12121997@gmail.com>
2024-04-06 12:00:44 +0530

    Client Side Routing

--- src/app/navigation/page.tsx
+++ src/app/navigation/page.tsx
@@ -1,9 +1,23 @@
+'use client';
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react';
 
 const NavigationTest = () => {
+    const router = useRouter();
+    useEffect(()=>{
+        router.prefetch("/about") /* Used to prefetch the router and only works on the Prod*/
+    },[])
   return (
-  <div className="flex gap-4">
+  <div className="flex flex-wrap gap-4 mx-[3%]">
     <Link href="/" prefetch={false} className='bg-text-main text-main p-1'>Home without prefetch</Link>
+    <button className="bg-text-main text-main px-2 py-1" onClick={()=>router.push("/")}>Home (PUSH)</button>
+    <button className="bg-text-main text-main px-2 py-1" onClick={()=>router.replace("/")}>Home (Replace)</button>
+    <button className="bg-text-main text-main px-2 py-1" onClick={()=>router.refresh()}>Refresh</button>
+    <button className="bg-text-main text-main px-2 py-1" onClick={()=>router.back()}>Back</button>
+    <button className="bg-text-main text-main px-2 py-1" onClick={()=>router.forward()}>Forward</button>
+    {/* <button className="bg-text-main text-main px-2 py-1" onClick={()=>router.prefetch("/")}>Home (Prefetch)</button> */}
+
   </div>
 
   )
