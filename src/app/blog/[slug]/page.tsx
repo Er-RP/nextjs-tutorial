0617f4db4fe6848c91c3365300f3091d972f9750
Rajapandi <rajapandi12121997@gmail.com>
2024-04-13 16:27:07 +0530

    Metadata dynamic

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,16 @@
 import { Suspense } from "react"
 import DummyUser from "./dummyUser"
 
+export const generateMetadata = async ({params}:any) =>{
+  const {slug} = params
+  const user = await getUser({id:slug})
+  return {
+    title : `${user?.name} view`,
+    description: user?.email
+  }
+}
+
+
 const getUser = async ({id}:{id:any}) => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
   if(!res.ok){
